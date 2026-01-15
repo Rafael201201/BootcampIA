ced8019de30808802b93fda9f98c1ed9b71511ea
Mather-v <bynpc04@gmail.com>
2026-01-15 10:52:17 -0500

    BootcampIA2

--- Personal/Mision1/proyecto1/entrenamiento.py
+++ Personal/Mision1/proyecto1/entrenamiento.py
@@ -1,2 +1,13 @@
+#librerias
 import re
+
+"""
+Expresiones regulares en python
+Problemas reales
+"""
+#codigo
 print("Libreria cargada crrectamente")
+#ejemplo1
+texto="Mi Numero es 12345"
+resultado=re.search(r"\d+",texto)
+print(resultado.group())
