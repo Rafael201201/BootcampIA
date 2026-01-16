425291320ec142bd3fb34ca8afcbf8cd5f25d4e6
Mather-v <bynpc04@gmail.com>
2026-01-16 08:31:20 -0500

    BootcampIA6

--- Personal/Mision1/proyecto1/entrenamiento.py
+++ Personal/Mision1/proyecto1/entrenamiento.py
@@ -11,6 +11,21 @@ print("Libreria cargada crrectamente")
 texto="Mi Numero es 12345"
 resultado=re.search(r"\d+",texto)
 print(f"{texto} Resultado {resultado.group()}")
+
 texto="Mi Numero es 12345-985"
 resultado=re.search(r"\d+",texto)
 print(f"{texto} Resultado {resultado.group()}")
+
+texto="Mi Numero es 12345-985"
+resultado=re.findall(r"\d+",texto)
+print(f"{texto} Resultado {resultado}")
+
+texto="Mi Numero es 123*45-985"
+resultado=re.findall(r"\d+",texto)
+print(f"{texto} Resultado {resultado}")
+
+documento1="cc.75.055.60"
+
+def clean_id(documento):
+    return re.sub(r"\D","",documento)
+print(clean_id(documento1))
