b5ed07292243d2369ea763f1022c6cb0df13b034
Mather-v <bynpc04@gmail.com>
2026-01-21 13:03:13 -0500

    BootcampIA

--- Personal/Mision3/version1/chatbot_sp.py
+++ Personal/Mision3/version1/chatbot_sp.py
@@ -34,13 +34,25 @@ def predict_answer(model, verctorizer,unique_answers,user_text):
 # Programa principal
 if __name__ == "__main__":
     training_data =[
-    ("hola","!Hola¡ ¿En que puedo ayudarte?")
+    ("hola","!Hola¡ ¿En que puedo ayudarte?"),
     ("hola", "¡Hola! ¿En qué podemos ayudarte hoy?"),
-    ("buenos días", "Buenos días, gracias por contactarnos. ¿Cómo podemos asistirte?"),
+    ("buenos dias", "Buenos días, gracias por contactarnos. ¿Cómo podemos asistirte?"),
     ("buenas tardes", "Buenas tardes, es un gusto atenderte. ¿Qué consulta tienes?"),
     ("buenas noches", "Buenas noches, estamos a tu disposición. ¿En qué podemos ayudarte?"),
     ("información", "Con gusto te brindamos la información que necesitas. ¿Sobre qué tema?"),
     ("soporte", "Nuestro equipo de soporte está listo para ayudarte. Cuéntanos tu inconveniente."),
     ("precio", "Con gusto te compartimos nuestros precios. ¿Qué servicio te interesa?"),
     ("gracias", "Gracias a ti por comunicarte con nosotros. ¡Que tengas un excelente día!")
+
     ]
+    model,vectorizer,unique_answers=build_and_train_model(training_data)
+    #Mostrar un mensaje inicial al usuario 
+    print("Chatbot supervisado listo,Escribe Salir para terminar.\n")
+    while True:
+        #Pedimos una frase al usuario
+        user =input("Tú: ").strip()
+        if user.lower() in {"Salir","exit","quit"}:
+            print("Bot: !Hasta pronto¡")
+            break
+        response=predict_answer(model,vectorizer,unique_answers,user)
+        print("Bot: ",response)
