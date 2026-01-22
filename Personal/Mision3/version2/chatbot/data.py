b950e80a26d1b8eb617e7ac32a102b58d33e8c3f
Mather-v <bynpc04@gmail.com>
2026-01-22 08:17:13 -0500

    V2

---
+++ Personal/Mision3/version2/chatbot/data.py
@@ -0,0 +1,12 @@
+training_data =[
+    ("hola","!Hola¡ ¿En que puedo ayudarte?"),
+    ("hola", "¡Hola! ¿En qué podemos ayudarte hoy?"),
+    ("buenos dias", "Buenos días, gracias por contactarnos. ¿Cómo podemos asistirte?"),
+    ("buenas tardes", "Buenas tardes, es un gusto atenderte. ¿Qué consulta tienes?"),
+    ("buenas noches", "Buenas noches, estamos a tu disposición. ¿En qué podemos ayudarte?"),
+    ("información", "Con gusto te brindamos la información que necesitas. ¿Sobre qué tema?"),
+    ("soporte", "Nuestro equipo de soporte está listo para ayudarte. Cuéntanos tu inconveniente."),
+    ("precio", "Con gusto te compartimos nuestros precios. ¿Qué servicio te interesa?"),
+    ("gracias", "Gracias a ti por comunicarte con nosotros. ¡Que tengas un excelente día!")
+
+    ]
---
+++ Personal/Mision3/version2/chatbot/model.py
@@ -0,0 +1,70 @@
+#scikit-learn
+import os
+import pickle 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+MODEL_DIR = "models"
+MODEL_PATH = os.path.join(MODEL_DIR,"model.pkl")
+VECTORIZER_PATH = os.path.join(MODEL_DIR,"vectorizer.pkl")
+ANSWERS_PATH = os.path.join(MODEL_DIR,"answer.pkl")
+
+#Funcion de entrenamiento preguntas y respuestas
+def build_and_train_model(train_pairs):
+    #train_pairs lista de pares(pregunta,respuestas)
+    #ejemplo [("hola","!Hola¡"),("adios","!Hasta luego¡")]
+    #separamos las preguntas y respuestas en dos listas
+    questions =[q for q, _ in train_pairs]# lista de preguntas
+    answers =[a for _, a in train_pairs]# lista de respuestas
+    # creamos el vectorizado, que traducira el texto a numeros
+    vectorizer=CountVectorizer()
+    #Entrenamiento
+    x = vectorizer.fit_transform(questions)
+    # obtenemos una lista de respuestas unica
+    unique_answers = sorted(set(answers))
+    # crear el diccionario  con las etiquetas
+    answer_to_label={a: i for i, a in enumerate(unique_answers)}
+    #creamos una lista
+    y=[answer_to_label[a] for a in answers]
+    #Modelo clasificacion de texto
+    model = MultinomialNB()
+    #Entrenar el modelo
+    model.fit(x,y)
+    #crear carpeta para guardar el model si no existe
+    os.makedirs(MODEL_DIR,exist_ok=True)
+    #Guardar los objetos entrenados 
+    with open(MODEL_DIR,"wb") as f:
+        pickle.dump(model,f)
+    return model,vectorizer,unique_answers
+# Funcion predict_answer
+def predict_answer(model, verctorizer,unique_answers,user_text):
+    # convertimos el texto a numeros
+    x = verctorizer.transform([user_text])
+    # el modelo precide la etiqueta de la respuesta correcta
+    label = model.predict(x)[0]
+    return unique_answers[label]
+# Programa principal
+if __name__ == "__main__":
+    training_data =[
+    ("hola","!Hola¡ ¿En que puedo ayudarte?"),
+    ("hola", "¡Hola! ¿En qué podemos ayudarte hoy?"),
+    ("buenos dias", "Buenos días, gracias por contactarnos. ¿Cómo podemos asistirte?"),
+    ("buenas tardes", "Buenas tardes, es un gusto atenderte. ¿Qué consulta tienes?"),
+    ("buenas noches", "Buenas noches, estamos a tu disposición. ¿En qué podemos ayudarte?"),
+    ("información", "Con gusto te brindamos la información que necesitas. ¿Sobre qué tema?"),
+    ("soporte", "Nuestro equipo de soporte está listo para ayudarte. Cuéntanos tu inconveniente."),
+    ("precio", "Con gusto te compartimos nuestros precios. ¿Qué servicio te interesa?"),
+    ("gracias", "Gracias a ti por comunicarte con nosotros. ¡Que tengas un excelente día!")
+
+    ]
+    model,vectorizer,unique_answers=build_and_train_model(training_data)
+    #Mostrar un mensaje inicial al usuario 
+    print("Chatbot supervisado listo,Escribe Salir para terminar.\n")
+    while True:
+        #Pedimos una frase al usuario
+        user =input("Tú: ").strip()
+        if user.lower() in {"Salir","exit","quit"}:
+            print("Bot: !Hasta pronto¡")
+            break
+        response=predict_answer(model,vectorizer,unique_answers,user)
+        print("Bot: ",response)
